b1ecb5481765afc21b4bb70416d4350d889d6072
Sarah Lensing <sarah@mapzen.com>
2017-04-10 16:02:42 -0600

    map bg color example

--- Example/MapViewController.swift
+++ Example/MapViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 import TangramMap
 
-class MapViewController: TGMapViewController {
+class MapViewController: TGMapViewController, TGMapViewDelegate {
+
+  var loaded = false
 
   init() {
     super.init(nibName: nil, bundle: nil)
@@ -19,9 +21,23 @@ class MapViewController: TGMapViewController {
     fatalError("init(coder:) has not been implemented")
   }
 
-  override func viewWillAppear(_ animated: Bool) {
-    let sceneURL = "https://tangrams.github.io/walkabout-style/walkabout-style.yaml"
-    super.loadSceneFileAsync(sceneURL, sceneUpdates: [ TGSceneUpdate(path: "global.sdk_mapzen_api_key", value: "vector-tiles-tyHL4AY") ])
+  override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+    super.glkView(view, drawIn: rect)
+    if(!loaded) {
+      glClearColor(1.0, 0.0, 0.1, 1.0);
+      glClear(GLbitfield(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
+    }
   }
 
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+
+    let sceneURL = "https://tangrams.github.io/walkabout-style/walkabout-style.yaml"
+    self.mapViewDelegate = self
+    super.loadSceneFileAsync(sceneURL, sceneUpdates: [ TGSceneUpdate(path: "global.sdk_mapzen_api_key", value: "vector-tiles-tyHL4AY")])
+  }
+
+  func mapView(_ mapView: TGMapViewController, didLoadSceneAsync scene: String) {
+    loaded = true
+  }
 }
